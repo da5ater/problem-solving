38eddd297ede9c164336ba4b9fc49a2c2ca76d57
da5ater <vincentvan205ater@gmail.com>
2024-07-30 09:53:46 +0300

    chnage the file structure

--- hacker ranck/array/dynamic_arr.cpp
+++ hacker ranck/array/dynamic_arr.cpp
@@ -5,29 +5,37 @@ using namespace std;
 
 int main()
 {
+    //-------------------------
     // int n, q;
     // cin >> n >> q;
     int n = 2;
     int q = 5;
+    //-------------------------
 
     vector<vector<int>> arr(n);
 
     int last_answer = 0;
 
+    //---------------------------------
     vector<vector<int>> queries = {
         {1, 0, 5},
         {1, 1, 7},
         {1, 0, 3},
         {2, 1, 0},
         {2, 1, 1}};
-
+    //------------------------------------
     for (int row = 0; row < q; row++)
     {
-        if (queries[row][0] == 1)
+        for (int column = 0; column < 3; column++)
         {
-                }
-        else
-        {
+            if (queries[row][0] == 1)
+            {
+                cout << 1;
+            }
+            else
+            {
+                cout << 2;
+            }
         }
     }
 }
