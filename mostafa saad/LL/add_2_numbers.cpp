7b709fcbb67e4cd280412347edf5c84b7f3a2297
da5ater <vincentvan205ater@gmail.com>
2024-08-24 14:56:05 +0300

    solved the add 2 numbers problem after 2 sessions

--- mostafa saad/LL/add_2_numbers.cpp
+++ mostafa saad/LL/add_2_numbers.cpp
@@ -35,21 +35,59 @@ void append(ListNode*& head, int value) {
     }
 }
 
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* result = nullptr;
+    int sum = 0;
+    int rem = 0;
+
+    while (l2 || l1) {
+        if (!l2) {
+            sum = l1->val + rem;
+        } else if (!l1) {
+            sum = l2->val + rem;
+        } else {
+            sum = l2->val + l1->val + rem;
+        }
+        if (sum >= 10) {
+            sum = sum % 10;
+            rem = 1;
+        } else {
+            rem = 0;
+        }
+        append(result, sum);
+        if (!l1 && l2->next == nullptr && rem != 0 ||
+            !l2 && l1->next == nullptr && rem != 0 ||
+            l2 && !l2->next && l1 && !l1->next && rem != 0)
+            append(result, rem);
+
+        if (l2) {
+            l2 = l2->next;
+        }
+        (l1) ? l1 = l1->next : l1;
+    }
+    return result;
+}
+
 int main() {
     ListNode* head1 = nullptr;
-    append(head1, 2);
-    append(head1, 3);
-    append(head1, 4);
+    append(head1, 9);
+    append(head1, 9);
+    append(head1, 9);
+    append(head1, 9);
+    append(head1, 9);
+    append(head1, 9);
+    append(head1, 9);
 
     ListNode* head2 = nullptr;
-    append(head2, 5);
-    append(head2, 6);
-    append(head2, 4);
+    append(head2, 9);
+    append(head2, 9);
+    append(head2, 9);
+    append(head2, 9);
 
     printList(head1);
     printList(head2);
 
-    cout << 10 % 20;
+    printList(addTwoNumbers(head1, head2));
 
     return 0;
 }
