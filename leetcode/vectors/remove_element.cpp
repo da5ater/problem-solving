d97e2b0ff6c37656094ae1d414ba334b608c65bc
Mohamed <107060533+da5ater@users.noreply.github.com>
2024-10-15 03:57:41 +0300

    solved Remove Element
    leetcode -> array ->4

--- leetcode/vectors/remove_element.cpp
+++ leetcode/vectors/remove_element.cpp
@@ -4,19 +4,11 @@
 using namespace std;
 
 int removeElement(vector<int> &nums, int val) {
-  int k = nums.size();
+  int k = 0;
   for (int i = 0; i < nums.size(); i++) {
-    if (nums[i] == val) {
-      for (int j = i + 1; j < nums.size(); j++) {
-        if (nums[j] != val) {
-          swap(nums[i], nums[j]);
-          k--;
-          break;
-        }
-      }
-    } else if (nums[i] != val) {
-
-      k--;
+    if (nums[i] != val) {
+      swap(nums[i], nums[k]);
+      k++;
     }
   }
   return k;
